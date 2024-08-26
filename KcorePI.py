4413d2992f34f840b6499374212712c49b0ead6c
kennet <kennet1one@gmail.com>
2024-08-26 19:45:21 +0200

    up

--- KcorePI.py
+++ KcorePI.py
@@ -1,12 +1,13 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
+with open('token.txt', 'r') as file:
+    token = file.read().strip()
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
-
-app = ApplicationBuilder().token("jjjj").build()
+app = ApplicationBuilder().token(token).build()
 
 app.add_handler(CommandHandler("hello", hello))
 
